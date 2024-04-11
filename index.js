b473d460cc788a7f7745a8ca7516bd91053486e7
Luiz Felipe <luizfelipe01700@gmail.com>
2024-04-11 19:58:04 -0300

    adicionei o controler do register de usuario

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use("/", reUserControler);
 
 app.get("/", (req, res) => {
     res.render("home");
---
+++ registerUser/reUserControler.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/registerUser", (req, res) => {
+    res.render("registerUser");
+})
+
+module.exports = router;
