501bddc5ffe3dcc16ec84d618c244a1b68486953
Luiz Felipe <luizfelipe01700@gmail.com>
2024-04-11 02:07:21 -0300

    terminei o carrossel principal

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
     res.render("home");
 })
 
+app.get("/about", (req,res) => {
+    res.render("about");
+})
+
 app.listen(8080, () => {
     console.log("Server on");
 })
