3f667a4c883b089646f56914eaab1d11423da432
Luiz Felipe <luizfelipe01700@gmail.com>
2024-05-20 16:39:58 -0300

    painel administrativo

--- adminControler/adminControler.js
+++ adminControler/adminControler.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const users = require("./Reuser");
+const events = require("../registerEvent/ReEvent");
 
 router.get("/logAdm", (req, res) => {
     res.render("login");
